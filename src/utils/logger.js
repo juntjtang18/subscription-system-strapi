ab65979a604639fc1d17048a5ac2b29719014f09
jamestjtang <juntjtang@163.com>
2025-07-26 09:58:12 -0700

    refine logger.js

--- src/utils/logger.js
+++ src/utils/logger.js
@@ -1,12 +1,12 @@
 'use strict';
 
 // Read the log level from the environment variable. Default to 'info'.
-const logLevel = process.env.SUBSYS_LOG_LEVEL || 'info';
+const logLevel = process.env.LOG_LEVEL || 'info';
 
 /**
  * A simple logger utility that logs messages based on the SUBSYS_LOG_LEVEL.
- * - 'debug' level logs everything (debug, info, error).
- * - 'info' level logs info and errors.
+ * - 'debug' level logs everything (debug, info, warn, error).
+ * - 'info' level logs info, warn, and errors.
  * - Any other level will only log errors.
  */
 const logger = {
@@ -27,6 +27,16 @@ const logger = {
   info: (...args) => {
     if (logLevel === 'debug' || logLevel === 'info') {
       console.log(...args);
+    }
+  },
+
+  /**
+   * Logs a warning message if the level is 'info' or 'debug'.
+   * @param {...any} args - The messages or objects to log.
+   */
+  warn: (...args) => {
+    if (logLevel === 'debug' || logLevel === 'info') {
+      console.warn(...args);
     }
   },
 
